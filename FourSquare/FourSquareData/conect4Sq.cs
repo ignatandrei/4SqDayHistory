b8f74a263007d79b8230fd6bb86d548a446e45e6
Andrei Ignat <ignatandrei@yahoo.com>
2014-01-16 23:55:05 +0200

    first version

---
+++ FourSquare/FourSquareData/conect4Sq.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using FourSquare.SharpSquare.Core;
+using FourSquare.SharpSquare.Entities;
+
+namespace FourSquareData
+{
+   public class conect4Sq
+{
+    // Fields
+    private SharpSquare sharpSquare;
+    private string thisRequest = Guid.NewGuid().ToString("N");
+    private string url = "http://fsq.apphb.com/";
+
+    // Methods
+    public conect4Sq(string clientId, string clientSecret)
+    {
+        this.sharpSquare = new SharpSquare(clientId, clientSecret);
+    }
+
+    public void Authenticate()
+    {
+        string urlAuth = this.sharpSquare.GetAuthenticateUrl(this.url + "redirect/" + this.thisRequest);
+        Process p = new Process();
+        p.StartInfo.FileName = urlAuth;
+        p.Start();
+        p.WaitForExit();
+        Task<string> q = new WebClient().DownloadStringTaskAsync(this.url + "Values/" + this.thisRequest);
+        string token = this.sharpSquare.GetAccessToken("url", q.Result);
+    }
+
+    private static long ToUnixTime(DateTime date)
+    {
+        DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        TimeSpan ts = date.ToUniversalTime() - epoch;
+        return Convert.ToInt64(ts.TotalSeconds);
+    }
+
+    public List<VenueHistory> VenuesBetween(DateTime? afterTimestamp, DateTime? beforeTimestamp)
+    {
+        Dictionary<string, string> d = new Dictionary<string, string>();
+        if (beforeTimestamp.HasValue)
+        {
+            d.Add("beforeTimestamp", ToUnixTime(beforeTimestamp.Value).ToString());
+        }
+        if (afterTimestamp.HasValue)
+        {
+            d.Add("afterTimestamp", ToUnixTime(afterTimestamp.Value).ToString());
+        }
+        if (d.Count == 0)
+        {
+            d = null;
+        }
+        return this.sharpSquare.GetUserVenueHistory(d);
+    }
+}
+
+ 
+
+}
---
+++ FourSquare/FourSquareWeb/Controllers/ValuesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace FourSquareWeb.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        public string GetData(string id)
+        {
+            return System.Web.HttpContext.Current.Application[id].ToString();
+        }
+    }
+}
