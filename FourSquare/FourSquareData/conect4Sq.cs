3079add69b7c0fd5180f511300b9fb6c1a7fee1c
Andrei Ignat <ignatandrei@yahoo.com>
2014-01-17 02:35:49 +0200

    works

--- FourSquare/FourSquareData/conect4Sq.cs
+++ FourSquare/FourSquareData/conect4Sq.cs
@@ -4,9 +4,11 @@ using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using FourSquare.SharpSquare.Core;
 using FourSquare.SharpSquare.Entities;
+using Microsoft.Win32;
 
 namespace FourSquareData
 {
@@ -25,14 +27,46 @@ namespace FourSquareData
 
     public void Authenticate()
     {
-        string urlAuth = this.sharpSquare.GetAuthenticateUrl(this.url + "home/redirect4Sq/" + this.thisRequest);
+        string urlAuth = this.sharpSquare.GetAuthenticateUrl(this.url + "home/redirect4sq/" + this.thisRequest);
         Process p = new Process();
-        p.StartInfo.FileName = urlAuth;
+        p.StartInfo.FileName = getDefaultBrowser();
+        p.StartInfo.Arguments = urlAuth;
         p.Start();
-        p.WaitForExit();
-        Task<string> q = new WebClient().DownloadStringTaskAsync(this.url + "api/Values/ClientToken/" + this.thisRequest);
-        string token = this.sharpSquare.GetAccessToken("url", q.Result);
+        Thread.Sleep(5000);
+        var newUrl = this.url + "api/Values/ClientToken/" + this.thisRequest;
+        Task<string> q = new WebClient().DownloadStringTaskAsync(newUrl);
+        var code = q.Result.Replace("\"","");
+        string token = this.sharpSquare.GetAccessToken(this.url + "home/redirect4sq/", code);
     }
+    private string getDefaultBrowser()
+    {
+        string browser = string.Empty;
+        RegistryKey key = null;
+        try
+        {
+            key = Registry.ClassesRoot.OpenSubKey(@"HTTP\shell\open\command", false);
+
+            //trim off quotes
+            browser = key.GetValue(null).ToString().ToLower().Replace("\"", "");
+            if (!browser.EndsWith("exe"))
+            {
+                //get rid of everything after the ".exe"
+                browser = browser.Substring(0, browser.LastIndexOf(".exe") + 4);
+            }
+        }
+        finally
+        {
+            if (key != null) key.Close();
+        }
+        return browser;
+    }
+
+    public static DateTime FromUnixTime(long unixTime)
+    {
+        var dt = new DateTime(621355968000000000L, DateTimeKind.Utc);
+        return dt + TimeSpan.FromSeconds(unixTime);
+    }
+
 
     private static long ToUnixTime(DateTime date)
     {
@@ -40,9 +74,42 @@ namespace FourSquareData
         TimeSpan ts = date.ToUniversalTime() - epoch;
         return Convert.ToInt64(ts.TotalSeconds);
     }
+    public List<Checkin> CheckinsBetween(DateTime? afterTimestamp, DateTime? beforeTimestamp)
+    {
+        Dictionary<string, string> d = new Dictionary<string, string>();
+        if (beforeTimestamp.HasValue)
+        {
+            d.Add("beforeTimestamp", ToUnixTime(beforeTimestamp.Value).ToString());
+        }
+        if (afterTimestamp.HasValue)
+        {
+            d.Add("afterTimestamp", ToUnixTime(afterTimestamp.Value).ToString());
+        }
+        if (d.Count == 0)
+        {
+            d = null;
+        }
+        return this.sharpSquare.GetUserCheckins("self",d);
+    }
+    public List<Checkin> CheckinsToday()
+    {
+        var now = DateTime.Now.Date;
+        return CheckinsBetween(now, DateTime.Now.Date.AddDays(1));
+    }
+    public List<Checkin> CheckinsYesterday()
+    {
+        var now = DateTime.Now.Date.AddDays(-1);
+        return CheckinsBetween(now, DateTime.Now.Date.AddDays(1));
+    }
     public List<VenueHistory> VenuesToday()
     {
         var now=DateTime.Now.Date;
+
+        return VenuesBetween(now, DateTime.Now.Date.AddDays(1));
+    }
+    public List<VenueHistory> VenuesYesterday()
+    {
+        var now = DateTime.Now.Date.AddDays(-1);
 
         return VenuesBetween(now, DateTime.Now.Date.AddDays(1));
     }
